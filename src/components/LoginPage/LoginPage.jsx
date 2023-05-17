1fbb6277a6b5feef92c9fe2524d6d1ac2acea366
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-16 21:23:41 -0500

    Added material ui components, app bar and card for login.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,64 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
-      <LoginForm />
+    <Grid 
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <Card item sx={{ minWidth: 340, m: 5}}>
+        <CardContent>
+          <Typography variant="h5" component="div" sx={{ mb: 1 }}>
+            Sign In
+          </Typography>
+          <TextField
+            id="outlined-helperText"
+            label="Username"
+            defaultValue=""
+            sx={{ m: 1, width: '30ch' }}
+          />
+          <br />
+          <TextField
+            id="outlined-helperText"
+            label="Password"
+            defaultValue=""
+            sx={{ mt: 1, mr: 1, ml: 1, width: '30ch' }}
+          />
+        </CardContent>
+        <CardActions>
+          <Button size="medium" variant="contained" sx={{ mr: 1, mb: 1, ml: 1 }} >Login</Button>
+          <Button size="medium" variant="contained" sx={{ mr: 1, mb: 1 }} >Register</Button>
+        </CardActions>
+      </Card>
+    </Grid>
+    // <div>
+    //   <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+    //   <center>
+    //     <button
+    //       type="button"
+    //       className="btn btn_asLink"
+    //       onClick={() => {
+    //         history.push('/registration');
+    //       }}
+    //     >
+    //       Register
+    //     </button>
+    //   </center>
+    // </div>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,44 +3,129 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            PC List
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              { 
+                !user.id && (
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>Login</MenuItem>
+                  </Menu>
+                )
+              }
+              { 
+                user.id && (
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                  </Menu>
+                )
+              }
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+    // <div className="nav">
+    //   <Link to="/home">
+    //     <h2 className="nav-title">Prime Solo Project</h2>
+    //   </Link>
+    //   <div>
+    //     {/* If no user is logged in, show these links */}
+    //     {!user.id && (
+    //       // If there's no user, show login/registration links
+    //       <Link className="navLink" to="/login">
+    //         Login / Register
+    //       </Link>
+    //     )}
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+    //     {/* If a user is logged in, show these links */}
+    //     {user.id && (
+    //       <>
+    //         <Link className="navLink" to="/user">
+    //           Home
+    //         </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
+    //         <Link className="navLink" to="/info">
+    //           Info Page
+    //         </Link>
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
+    //         <LogOutButton className="navLink" />
+    //       </>
+    //     )}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
+    //     <Link className="navLink" to="/about">
+    //       About
+    //     </Link>
+    //   </div>
+    // </div>
   );
 }
 
