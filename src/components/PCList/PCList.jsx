6d2416c8912c05f48631f36653fc5a758453f4d4
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-17 19:38:43 -0500

    Added an add pc button to pc list page. It doesnt do anything yet.

--- src/components/PCList/PCList.jsx
+++ src/components/PCList/PCList.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 function PCList() {
   const history = useHistory();
 
+  const addPC = () => {
+
+  }
+
   return (
     <div className="container">
-      
+      <Button variant="contained"
+        endIcon={<AddIcon />} 
+        color="secondary" 
+        onClick={addPC}
+        sx={{ margin: 0,
+          top: 'auto',
+          right: 20,
+          bottom: 'auto',
+          left: 'auto',
+          position: 'fixed',
+          padding: 1.3,
+        }}
+      >Add PC</Button>
     </div>
   );
 }
