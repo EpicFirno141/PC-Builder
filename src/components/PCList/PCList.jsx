2525d0ec715d9eb4ed9b78161298b573e5108d58
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-17 20:34:39 -0500

    Add PC button now adds a PC to the database.

--- src/components/PCList/PCList.jsx
+++ src/components/PCList/PCList.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import { useDispatch, useSelector } from 'react-redux';
 
 function PCList() {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const pcList = useSelector(store => store.pcList)
 
   const addPC = () => {
-
+    dispatch({ type: 'ADD_PC' });
   }
 
   return (
