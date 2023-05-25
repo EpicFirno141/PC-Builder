cc9e2a5a7221930bc08eb67f1f659914bfb7e02e
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-25 02:39:42 -0500

    Changed look of the add PC button.

--- src/components/PCList/PCList.jsx
+++ src/components/PCList/PCList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import Button from '@mui/material/Button';
+import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import Grid from '@mui/material/Grid';
 import PCItem from './PCItem';
@@ -21,19 +21,11 @@ function PCList() {
 
   return (
     <div className="container">
-      <Button variant="contained"
-        endIcon={<AddIcon />} 
-        color="secondary" 
-        onClick={addPC}
-        sx={{ margin: 0,
-          top: 'auto',
-          right: 20,
-          bottom: 'auto',
-          left: 'auto',
-          position: 'fixed',
-          padding: 1.3,
-        }}
-      >Add PC</Button>
+      <Fab color="secondary" onClick={addPC}
+        sx={{ margin: 0, position: 'absolute', bottom: 16, right: 16}}
+      >
+        <AddIcon />
+      </Fab>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {pcList.map((pc, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
