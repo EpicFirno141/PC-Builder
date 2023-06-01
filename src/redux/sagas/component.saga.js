2bf6cce83f0b9151e910945dca458196bd8904b3
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-31 20:33:26 -0500

    Created saga functions for fetching each type of component.

--- src/redux/sagas/component.saga.js
+++ src/redux/sagas/component.saga.js
@@ -49,11 +49,71 @@ function* cpuFetch() {
   }
 }
 
+function* coolerFetch() {
+  try {
+    const response = yield axios.get('/api/component/cooler');
+    yield put({ type: 'SET_COOLER_LIST', payload: response.data });
+  } catch (error){
+    console.log('Cooler GET request failed', error);
+  }
+}
+
+function* moboFetch() {
+  try {
+    const response = yield axios.get('/api/component/mobo');
+    yield put({ type: 'SET_MOBO_LIST', payload: response.data });
+  } catch (error){
+    console.log('Mobo GET request failed', error);
+  }
+}
+
+function* psuFetch() {
+  try {
+    const response = yield axios.get('/api/component/psu');
+    yield put({ type: 'SET_PSU_LIST', payload: response.data });
+  } catch (error){
+    console.log('PSU GET request failed', error);
+  }
+}
+
+function* memoryFetch() {
+  try {
+    const response = yield axios.get('/api/component/memory');
+    yield put({ type: 'SET_MEMORY_LIST', payload: response.data });
+  } catch (error){
+    console.log('Memory GET request failed', error);
+  }
+}
+
+function* storageFetch() {
+  try {
+    const response = yield axios.get('/api/component/storage');
+    yield put({ type: 'SET_STORAGE_LIST', payload: response.data });
+  } catch (error){
+    console.log('Storage GET request failed', error);
+  }
+}
+
+function* caseFetch() {
+  try {
+    const response = yield axios.get('/api/component/case');
+    yield put({ type: 'SET_CASE_LIST', payload: response.data });
+  } catch (error){
+    console.log('Case GET request failed', error);
+  }
+}
+
 function* componentSaga() {
   yield takeLatest('ADD_COMPONENT', addComponent);
   yield takeLatest('FETCH_COMPONENT_LIST', fetchComponentList);
   yield takeLatest('FETCH_GPU_LIST', gpuFetch);
   yield takeLatest('FETCH_CPU_LIST', cpuFetch);
+  yield takeLatest('FETCH_COOLER_LIST', coolerFetch);
+  yield takeLatest('FETCH_MOBO_LIST', moboFetch);
+  yield takeLatest('FETCH_PSU_LIST', psuFetch);
+  yield takeLatest('FETCH_MEMORY_LIST', memoryFetch);
+  yield takeLatest('FETCH_STORAGE_LIST', storageFetch);
+  yield takeLatest('FETCH_CASE_LIST', caseFetch);
   yield takeLatest('REMOVE_COMPONENT', removeComponent);
 }
 
